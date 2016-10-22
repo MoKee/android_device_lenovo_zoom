86cf98b85283176081b6a7e2153682ebd3ced074
martincz <martincz@mokeedev.com>
2016-10-22 20:24:15 +0800

    zoom: fix wrong kernel path
    Change-Id: I5a365a1ab7c21c1fb1b6dc3c5a2356d588ddf05d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -51,7 +51,7 @@ BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_APPEND_DTB := true
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
-TARGET_KERNEL_SOURCE := kernel/lenovo/LA.BR.1.2.7_rb1.33
+TARGET_KERNEL_SOURCE := kernel/lenovo/lenovo
 TARGET_KERNEL_CONFIG := cyanogenmod_zoom_defconfig
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 
